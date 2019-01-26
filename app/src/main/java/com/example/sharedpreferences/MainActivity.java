eea4a0032798e2c716ed58fb0f1afbed7dfa3d78
Jabriko <jabriko.banjarnegara@gmail.com>
2019-01-26 10:24:57 +0700

    test

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.sharedpreferences;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button applyTextButton, saveButton;
+    private Switch switch1;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String SWITCH1 = "switch1";
+
+    private String text;
+    private boolean switchOnOff;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        editText = findViewById(R.id.edittext);
+        applyTextButton = findViewById(R.id.apply_text_button);
+        saveButton = findViewById(R.id.save_button);
+        switch1 = findViewById(R.id.switch1);
+
+        applyTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savedata();
+            }
+        });
+
+        loadData();
+        updateViews();
+    }
+
+    private void updateViews() {
+        textView.setText(text);
+        switch1.setChecked(switchOnOff);
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+        switchOnOff = sharedPreferences.getBoolean(SWITCH1, false);
+    }
+
+    private void savedata() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(TEXT, textView.getText().toString());
+        editor.putBoolean(SWITCH1, switch1.isChecked());
+
+        editor.apply();
+
+        Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show();
+    }
+}
